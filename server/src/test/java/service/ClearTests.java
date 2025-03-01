2e7b92db31b690df5c806fad0f25a08113f91c7d
prestonhancock <PJHancock2@gmail.com>
2025-03-01 13:41:30 -0700

    Created test files

--- server/src/test/java/service/ClearTests.java
+++ server/src/test/java/service/ClearTests.java
@@ -1,13 +1,11 @@
 package service;
 
-import chess.ChessGame;
 import org.junit.jupiter.api.*;
-import passoff.model.*;
 
 public class ClearTests {
     @Test
     @DisplayName("Clear Positive Test")
     public void clearPositiveTest() {
-        ;
+
     }
 }
