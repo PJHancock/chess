f52b8c92df2f14af208ac535c9a9410bf0dcffc7
prestonhancock <PJHancock2@gmail.com>
2025-03-17 22:09:19 -0600

    Added repl loops

--- client/src/main/java/ui/Repl.java
+++ client/src/main/java/ui/Repl.java
@@ -3,6 +3,8 @@ package ui;
 import ui.Clients.GameplayClient;
 import ui.Clients.PostloginClient;
 import ui.Clients.PreloginClient;
+
+import java.util.Objects;
 import java.util.Scanner;
 import static ui.EscapeSequences.*;
 
@@ -18,19 +20,45 @@ public class Repl {
         gameplayClient = new GameplayClient(serverUrl);
     }
 
-    public void run() {
+    public void runPrelogin() {
         System.out.println(WHITE_PAWN + " Welcome to 240 chess. Type help to get started" + WHITE_PAWN);
         System.out.print(preloginClient.help());
-
         Scanner scanner = new Scanner(System.in);
         var result = "";
         while (!result.equals("quit")) {
-            printPrompt();
+            printPreloginPrompt();
             String line = scanner.nextLine();
 
             try {
                 result = preloginClient.eval(line);
                 System.out.print(SET_TEXT_COLOR_BLUE + result);
+                if (Objects.equals(line., "login") || Objects.equals(line, "register")) {
+                    System.out.print("Logged in as ");
+                    runPostlogin();
+                }
+            } catch (Throwable e) {
+                var msg = e.toString();
+                System.out.print(msg);
+            }
+            System.out.println();
+        }
+    }
+
+    public void runPostlogin() {
+        System.out.print(postloginClient.help());
+
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!(result.equals("logout") || result.equals("quit"))) {
+            printPostloginPrompt();
+            String line = scanner.nextLine();
+
+            try {
+                result = postloginClient.eval(line);
+                System.out.print(result);
+                if (Objects.equals(line., "join") || Objects.equals(line, "observe")) {
+                    runGameplay();
+                }
             } catch (Throwable e) {
                 var msg = e.toString();
                 System.out.print(msg);
@@ -39,24 +67,24 @@ public class Repl {
         System.out.println();
     }
 
-    public void runPrelogin() {
-        System.out.println(WHITE_PAWN + " Welcome to 240 chess. Type help to get started" + WHITE_PAWN);
-        System.out.print(preloginClient.help());
+    public void runGameplay() {
+        printGameboard();
+    }
 
-        Scanner scanner = new Scanner(System.in);
-        var result = "";
-        while (!result.equals("quit")) {
-            printPrompt();
-            String line = scanner.nextLine();
+    public void printGameboard() {
 
-            try {
-                result = preloginClient.eval(line);
-                System.out.print(SET_TEXT_COLOR_BLUE + result);
-            } catch (Throwable e) {
-                var msg = e.toString();
-                System.out.print(msg);
-            }
-        }
-        System.out.println();
+    }
+
+    private void printPreloginPrompt() {
+        System.out.print("\n" + "[LOGGED_OUT] >>> " + SET_TEXT_COLOR_GREEN);
+    }
+
+    private void printPostloginPrompt() {
+        System.out.print("\n" + "[LOGGED_OUT] >>> " + SET_TEXT_COLOR_BLUE);
+    }
+
+    private void printGameplayPrompt() {
+        // To be implemented layer
+        System.out.print("\n" + "[GamePlay Mode]" + ">>> ");
     }
 }
